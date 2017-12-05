85d214c46c38edca6a371a1a817478756c0b6604
rmccracken <rob.mccrackenjr@gmail.com>
2017-12-05 14:14:08 -0500

    new gif's replace old gif's

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,33 +12,47 @@ $.ajax({
 	method: "GET"
 }).done(function(response) {
 		console.log(response.data);
-
+		// removes previous gif's from div 
+		$("#animals").empty();
+		// sets the data from the response function to results.
 		var results = response.data;
 
 		for(var i = 0; i < results.length; i++) {
+			// sets up animalsDiv var to go into div id animals
 			var animalsDiv = $("#animals");
+			// sets animalImage to an image with the class of gif 
 			var animalImage = $("<img>").addClass("gif");
+
 			var rating = $("<p>").text("Rating: " + results[i].rating);
 			
-			animalsDiv.prepend(rating);
-		
+			
+			// sets the attriputes to the var anumalsDiv to start frozen then
 			animalImage.attr("src", results[i].images.fixed_height_small_still.url);
 			animalImage.attr("data-animate", results[i].images.fixed_height_small.url);
 			animalImage.attr("data-still", results[i].images.fixed_height_small_still.url);
 			animalImage.attr("data-state", "still");
-			
+			// puts the last image first
 			animalsDiv.prepend(animalImage);
-			makeButtons();
-	
+			// puts the gif rating above the gif
+			animalsDiv.prepend(rating);
 		}
-	});
-		// $(".gif").on("click", function(){
-		// 		var state = 
-		// });
-
-
+			// image class 
+			 $(".gif").on("click", function() {
+	        	var state = $(this).attr("data-state");
+	     
+	        if (state === "still") {
+	        	$(this).attr("src", $(this).attr("data-animate"));
+	        	$(this).attr("data-state", "animate"); 
+	        } else {
+	        	$(this).attr("src", $(this).attr("data-still"));
+	        	$(this).attr("data-state", "still");
+	        }
+	 
+	    });
+});
 
 }
+
 
  function makeButtons() {
 
